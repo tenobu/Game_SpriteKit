70073a0d87bfe53382b7d32913d28d5accbf936f
寺内 信夫 <tenobu@gmail.com>
2014-10-17 14:14:18 +0900

    Initial Commit

---
+++ Game_SpriteKit/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  Game_SpriteKit
+//
+
+//  Copyright (c) 2014年 寺内 信夫. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ Game_SpriteKit/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  Game_SpriteKit
+//
+
+//  Copyright (c) 2014年 寺内 信夫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
